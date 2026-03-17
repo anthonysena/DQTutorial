8aa7fac885adcfd57b19805c32ebcb9101e3f9d2
Anthony Sena <asena5@its.jnj.com>
2026-03-17 15:57:56 -0400

    Add code to capture dependencies and launch dashboard

---
+++ LaunchDqdDashboards.R
@@ -0,0 +1,11 @@
+DataQualityDashboard::viewDqDashboard(
+  jsonPath = normalizePath("synthea-characterization.json", winslash = "/", mustWork = TRUE)
+)
+
+DataQualityDashboard::viewDqDashboard(
+  jsonPath = normalizePath("synthea-convention.json", winslash = "/", mustWork = TRUE)
+)
+
+DataQualityDashboard::viewDqDashboard(
+  jsonPath = normalizePath("synthea-fatal.json", winslash = "/", mustWork = TRUE)
+)
